3125a48d187d3d5253b710482c7b307114fad262
Erik Johnson <erjohnson@blackberry.com>
2013-06-05 14:33:11 -0400

    Updating to proer syntax for send mail command now just requires a cron config

--- git-weekly-summary/send.sh
+++ git-weekly-summary/send.sh
@@ -4,11 +4,25 @@
 BRANCH=$1
 SUBJECT='Weekly Git Log Report'
 EMAIL='erik@erikjohnson.ca'
+TFILE="/tmp/git-weekly.$$.tmp"
+
+#MAke a temp file if it doesn't exist
+
+if [ ! -e "$TFILE" ] ; then
+    touch "$TFILE"
+fi
 
 # Update git and get the commit logs of the remote origin for the specific branch
 git fetch
-/bin/mail -s "$SUBJECT" "$EMAIL" < < git log origin/$BRANCH --since='monday' >> /tmp/git-commit-weekly.txt
 
-# Now let's do the real work on the email front
+echo 'Writing git commit history to temporary storage'
+git log origin/$BRANCH --since='last monday' >> $TFILE
 
+echo 'Mailing to' $EMAIL
+mail -s "$SUBJECT" --to "$EMAIL" < $TFILE
 
+echo 'Cleaning up'
+# Clenaup
+if [ -f "$TFILE" ] ; then
+    rm "$TFILE"
+fi
